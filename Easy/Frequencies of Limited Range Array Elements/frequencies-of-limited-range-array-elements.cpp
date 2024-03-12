d272da7a374e1247f1605d7629ff974dbe7c41f9
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2024-03-12 16:16:39 +0530

    Updated Solution - GfG to GitHub

--- Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.cpp
+++ Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.cpp
@@ -5,14 +5,12 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    //Function to count the frequency of all elements from 1 to N in the array.
     void frequencyCount(vector<int>& arr,int N, int P)
     { 
-        // code here
+        
           unordered_map<int, int> countMap;
           
-          // Iterate through the vector and update counts
-          
+
           for(int i=1; i<=N; i++){
               countMap[i]=0;
           }
